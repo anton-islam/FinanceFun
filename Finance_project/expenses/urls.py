dc25d793b7748de260f22426124fd7203b9d99e8
anton-islam <anton_islam@yahoo.com>
2023-02-22 13:18:06 -0500

    Link changes and page added!!

--- Finance_project/expenses/urls.py
+++ Finance_project/expenses/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('expenses/', views.expenses, name='expenses'),
+    path('icon/', views.icon, name='icon'),
 ]
--- Finance_project/expenses/views.py
+++ Finance_project/expenses/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'Homepage/index.html')
+def expenses(request):
+    return render(request,'Homepage/pages/Expenses/expenses.html')
+def icon(request):
+    return render(request,'Homepage/pages/icons/mdi.html')
